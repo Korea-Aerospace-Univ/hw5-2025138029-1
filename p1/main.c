4ddc0070c5f7e309325863e48fd403e1d3673e8a
2025138029 <seungjukim1124@gmail.com>
2026-04-29 16:25:57 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N = 0, net_money = 0, A_max = 0, B_max = 0, cnt = 0, i, j, k;
+    scanf("%d", &N);
+    net_money = N - 2600;
+    B_max = net_money/1500;
+    ///A_max = (net_money - (i * 1500))/900;
+    ///C_max = (net_money - (i * 1500) - (j * 900))/200;
+    
+    for(i = 1; i <= B_max; i++){
+        
+        A_max = (net_money - (i * 1500))/900;
+        
+        for(j = 1; j <= A_max; j++){
+            
+            
+            for(k = 1; (k < j) && (k < (i * 2)); k++){
+                
+                    
+                    if(((j * 900) + (i * 1500) + (k * 200)) == net_money)
+                        printf("%d %d %d", j + 1, (i * 2) + 2, k + 1);
+                        cnt += 1;
+            }
+        }
+    }
+    if(cnt == 0)
+        printf("none");
+    return 0;
+}
